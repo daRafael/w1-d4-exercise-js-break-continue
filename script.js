dd7bb7623e60575e94abe600fd1e401a44ec08df
Rafael Guerra <rafa99gm@hotmail.com>
2024-04-11 11:21:55 +0100

    solved

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Ready to go! 🚀");
+
+
+for (i = 0; i <= 20; i++) {
+
+  if(i !== 0 && i % 7 === 0) {
+    console.log(`${i} is divisible by 7!`)
+    continue;
+  }
+
+  if (i % 2 === 0) {
+    console.log(i, 'even');
+  } 
+  else if (i % 2 === 1) {
+    console.log(i, 'odd');
+  }
+}
